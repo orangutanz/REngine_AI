bf9b80520d08be5f2a84c343fa6f781d1d52db76
YuHan Ma <59942793+orangutanz@users.noreply.github.com>
2023-03-06 18:58:11 -0800

    Add EMath Lib

---
+++ Framework/EMath/EMath/Inc/Common.h
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+#include <math.h>
+#include <stdio.h>
+#include <vector>
+
+#define MathAssert(condition, message) \
+    do { \
+        if (! (condition)) { \
+            std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
+                      << " line " << __LINE__ << ": " << message << std::endl; \
+            std::exit(EXIT_FAILURE); \
+        } \
+    } while (false)
